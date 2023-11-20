dcb891ccf5657a90164162af5439cc5bd60c64aa
Trevor Evans <trevalexandro@gmail.com>
2023-11-19 23:42:27 -0600

    home page updates

--- src/components/lineage-graph.js
+++ src/components/lineage-graph.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import Tree from 'react-d3-tree';
 
 const LineageGraph = ({dependencies, repoName, onNodeClick}) => {
-    const children = dependencies.map(val => {
-        const {dependencyType} = val;
+    const [hoveringOverNode, setHoveringOverNode] = useState(false);
+    const [nodeKey, setNodeKey] = useState(undefined);
+
+    const children = dependencies.map((val, index) => {
+        const attributes = {};
+        if (hoveringOverNode && nodeKey === index) {
+            const {dependencyType} = val;
+            attributes.dependencyType = dependencyType;
+        }
         return {
-            attributes: {
-                dependencyType
-            },
+            key: index,
+            attributes,
             ...val
         };
     });
@@ -19,6 +26,19 @@ const LineageGraph = ({dependencies, repoName, onNodeClick}) => {
     const x = window.innerWidth / 2;
     const y = window.innerHeight / 3;
 
+    const toggleHoverState = (hovering, key) => {
+        if (!hovering && (hoveringOverNode || nodeKey)) {
+            setHoveringOverNode(false);
+            setNodeKey(undefined);
+            return;
+        }
+
+        if (hovering) {
+            setHoveringOverNode(true);
+            setNodeKey(key);
+        }
+    };
+
     return (
         <Tree 
             translate={{x, y}} 
@@ -29,6 +49,10 @@ const LineageGraph = ({dependencies, repoName, onNodeClick}) => {
             leafNodeClassName="node__leaf" 
             pathClassFunc={() => 'link'}
             onNodeClick={(node) => onNodeClick(node)}
+            onLinkMouseOut={(node) => toggleHoverState(false, node.data.key)}
+            onLinkMouseOver={(node) => toggleHoverState(true, node.data.key)}
+            onNodeMouseOut={(node) => toggleHoverState(false, node.data.key)}
+            onNodeMouseOver={(node) => toggleHoverState(true, node.data.key)}
             orientation='vertical'
         />
     )
--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,32 +1,57 @@
-import { Stack, Title, Text, AspectRatio, Overlay } from "@mantine/core";
+import { Stack, Title, Text, AspectRatio, Overlay, Space, Button } from "@mantine/core";
 import topSectionImage from '../images/DALL·E 2023-11-13 00.05.46 - connecting the dots and the colors are blue, white, and gray with a black background, digital art.png';
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { IconBrandGithubFilled } from "@tabler/icons-react";
 
 // dark hex #1a1b1e
 
 const TopSection = () => {
+    // TODO: Burger menu?
+    const [opened, {toggle}] = useDisclosure();
+    const mediaQueryMatches = useMediaQuery('(min-width: 767px)');
+
+    const onButtonClick = () => window.location = `${process.env.REACT_APP_OAUTH_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&scope=repo`;
+
     return (
-        <Stack gap='lg'>
-            <AspectRatio ratio={4 / 3}>
-                <Stack style={{zIndex: 2}}>
-                    <Title style={{fontSize: 75}}>
-                        See it all at once
-                    </Title>
-                    <Text size='xl'>
-                        A holistic view of your dependencies and their health.
-                    </Text>
-                </Stack>
-                <img src={topSectionImage} />
-                <Overlay fixed={true} gradient="linear-gradient(to right black, transparent)" opacity={0.99} />
-            </AspectRatio>
-        </Stack>
+        // TODO: Does this aspect ratio work for small screenn sizes?
+        <AspectRatio ratio={mediaQueryMatches ? 4 / 3 : 3 / 4}>
+            <Stack style={{zIndex: 201}}>
+                <Title style={{fontSize: 75, color: 'white'}}>
+                    See it all at once
+                </Title>
+                <Text size='xl' style={{color: 'white'}}>
+                    A holistic view of your dependencies and their health.
+                </Text>
+                <Button onClick={onButtonClick} rightSection={<IconBrandGithubFilled />}>
+                    Get started with GitHub
+                </Button>
+            </Stack>
+            <img src={topSectionImage} />
+            <Overlay fixed={true} gradient="linear-gradient(to right black, transparent)" opacity={0.99} />
+        </AspectRatio>
     );
 };
 
+const MidSection = () => {
+    return (
+        <Stack gap='lg'>
+            <Text size='xl' style={{color: 'white'}}>
+                Visual
+            </Text>
+            <Title style={{fontSize: 25, color: 'lime'}}>
+                Instead of digging through code & configurations, our simple UI allows you to see the whole picture instantly.
+            </Title>
+        </Stack>
+    );
+}
+
 const SplashPage = () => {
     return (
-        <Stack gap='xl'>
+        <Stack>
             <TopSection />
-            <Title>Test</Title>
+            <Space h='xl' />
+            <Space h='xl' />
+            <MidSection />
         </Stack>
     );
 };
